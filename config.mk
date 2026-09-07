88f233e8f1497ce2881e54e963458d3889be5069
h3nc4 <me@h3nc4.com>
2026-09-07 11:35:57 -0300

    add xwayland support

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 _VERSION = 0.8-dev
-VERSION  = `git describe --tags --dirty 2>/dev/null || echo $(_VERSION)`
+VERSION  = `echo $(_VERSION)`
 
 PKG_CONFIG = pkg-config
 
@@ -27,8 +27,8 @@ WLR_LIBS = `$(PKG_CONFIG) --libs wlroots-0.19`
 XWAYLAND =
 XLIBS =
 # Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
 
 # dwl itself only uses C99 features, but wlroots' headers use anonymous unions (C11).
 # To avoid warnings about them, we do not use -std=c99 and instead of using the
